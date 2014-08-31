6a5aa2799ed47a0b84c22f5dbf6db57677c39d8b
andraroston <andraroston@Andras-MacBook-Air.local>
2014-08-31 22:27:41 +0100

    Finished
    It works!

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 $(document).ready(function() {
 
-for (var num = 1; num < 101; num++) {
-	if(num % 3 === 0 && num % 5 === 0) {
-		console.log("fizzbuzz");
-		$("body").append("<p>" + "fizzbuzz" + "</p>");
-	}
-	else if(num % 3 === 0) {
-		console.log("fizz");
-		$("body").append("<p>" + "fizz" + "</p>");
-	}
-	else if(num % 5 === 0) {
-		console.log("buzz");
-		$("body").append("<p>" + "buzz" + "</p>");	
-	}
-	else {
-		console.log(num)
-		$("body").append("<p>" + num + "</p>");
-	}
-}
+	var humanInput;
 
+	humanInput = prompt ("Pick a number, any number!");
+		while(isNaN(humanInput) === true) {
+			humanInput = prompt ("You didn't pick a number! Try again.")
+		}
+		console.log(humanInput);
+
+		var fizzbuzz = function(choice) {	
+			for (var num = 1; num <= choice; num++) {
+				if(num % 3 === 0 && num % 5 === 0) {
+					console.log("fizzbuzz");
+					$("body").append("<p>" + "FizzBuzz" + "</p>");
+				}
+				else if(num % 3 === 0) {
+					console.log("fizz");
+					$("body").append("<p>" + "Fizz" + "</p>");
+				}
+				else if(num % 5 === 0) {
+					console.log("buzz");
+					$("body").append("<p>" + "Buzz" + "</p>");	
+				}
+				else {
+					console.log(num)
+					$("body").append("<p>" + num + "</p>");
+				}			
+		
+			}
+		}
+
+	fizzbuzz(humanInput);		
+	
 });
